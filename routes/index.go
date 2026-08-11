87c8c3f2c0937897c50d211045bab2f7772a389d
akatgelar <akatgelar@gmail.com>
2026-08-11 15:08:13 +0700

    fix: swagger redirect

--- routes/index.go
+++ routes/index.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"akatgelar/dashboard-bps-backend/models"
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -13,28 +15,40 @@ import (
 
 func RouteIndex(g *gin.RouterGroup) {
 
-	// root
+	appPort := os.Getenv("APP_PORT")
+	ginMode := os.Getenv("GIN_MODE")
+	host := ""
+	if ginMode == "debug" {
+		host = "localhost:" + appPort
+	} else if ginMode == "release" {
+		host = "https://dashboard-bps.akatgelar.app/api/"
+	}
+	fmt.Print("host")
+	fmt.Print(host)
+
+	// route "/" -> swagger
 	g.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, host + "/swagger/index.html")
+	})
+
+	// docs swagger served under /swagger/*
+	ServiceSwagger.SwaggerInfo.Title = "Dashboard BPS API"
+	ServiceSwagger.SwaggerInfo.Description = "This is a documentation for Dashboard BPS API."
+	ServiceSwagger.SwaggerInfo.Version = "2.0"
+	ServiceSwagger.SwaggerInfo.Host = host
+	ServiceSwagger.SwaggerInfo.BasePath = "/"
+	ServiceSwagger.SwaggerInfo.Schemes = []string{"http", "https"}
+	g.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// root
+	g.GET("/is_alive", func(c *gin.Context) {
 		c.JSON(
 			200,
 			models.BaseResponse{
 				Status:  true,
 				Message: "Welcome to Dashboard BPS API",
-				Data: gin.H{
-					"swagger": "/docs/index.html",
-				},
+				Data:    gin.H{},
 			},
 		)
 	})
-
-	// docs swagger
-	appPort := os.Getenv("APP_PORT")
-	ServiceSwagger.SwaggerInfo.Title = "Swagger Dashboard BPS API"
-	ServiceSwagger.SwaggerInfo.Description = "This is a documentation for Dashboard BPS API."
-	ServiceSwagger.SwaggerInfo.Version = "1.0"
-	ServiceSwagger.SwaggerInfo.Host = "localhost:" + appPort
-	ServiceSwagger.SwaggerInfo.BasePath = "/"
-	ServiceSwagger.SwaggerInfo.Schemes = []string{"http", "https"}
-	g.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
 }
